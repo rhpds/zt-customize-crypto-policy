66e3533d94bbbbda5ee4fc9721a409b83d5e7ad1
Matt Micene (RHT) <mmicene@redhat.com>
2025-07-11 13:43:31 -0400

    Update UI bundle and RHEL 10 image config, move package install

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Install the mod_ssl tools to autogen a self-signed cert at start up
-dnf install -y httpd mod_ssl
+# Install the mod_ssl moved to instance.yml
+# dnf install -y httpd mod_ssl
 systemctl --now enable httpd
 
 # Once we have a SSL cert created at DEFAULT 2048 bit, we can up the policy for the lab
